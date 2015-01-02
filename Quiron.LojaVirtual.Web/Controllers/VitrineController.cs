045c1a5328ac672e3f6b40d72657fc33ee9a4fae
GleidsonLimaBsb <gleidson_lima@hotmail.com>
2015-01-02 15:49:07 -0200

    ok

---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+
+        //Crio uma variável
+        private ProdutosRepositorio _repositorio;
+        public int ProdutosPorPagina = 3;
+        
+        // GET: Vitrine
+        public ActionResult ListaProdutos(int pagina = 1)
+        {
+
+            _repositorio = new ProdutosRepositorio();
+            //var produtos = _repositorio.Produtos.Take(10); //Nesta linha o Take serve para trazer apenas 10 registros
+            var produtos = _repositorio.Produtos    //Nesta linha retorno uma coleção de produtos
+                .OrderBy(p => p.Descricao)         //Nessa linha orderno pelA Descrição
+                .Skip((pagina - 1) * ProdutosPorPagina)
+                .Take(ProdutosPorPagina);
+
+            return View(produtos);
+        }
+    }
+}
